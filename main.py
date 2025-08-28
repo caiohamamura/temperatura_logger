d0fb86193435d5ba4c1f17826ece2ffeaeb42d9d
Caio Hamamura <caiohamamura@gmail.com>
2025-08-28 16:53:10 -0300

    Updated to use MQTT

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-from fastapi import FastAPI, Query, WebSocket
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-from datetime import datetime, timedelta, timezone
+import os
+from datetime import datetime, timezone
 from typing import List, Dict, Deque
 from collections import defaultdict, deque
+import json
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-import random
+import paho.mqtt.client as mqtt
 
+
+load_dotenv()
+
+hivemq_user = os.getenv('HIVEMQ_USER')
+hivemq_pass = os.getenv('HIVEMQ_PASSWORD')
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/chbr_topic")
+
+def on_message(client, userdata, msg):
+    '''
+    The callback for when the client receives a CONNACK response from the server.
+    '''
+    now = datetime.now(timezone.utc)
+
+    msg_dict = json.loads(msg.payload)
+    for endereco, temperatura in msg_dict.items():
+        if len(storage[endereco]) > 2:
+            penultima_temp = storage[endereco][-2][1]
+            ultima_temp = storage[endereco][-1][1]
+            if temperatura == penultima_temp and temperatura == ultima_temp:
+                storage[endereco][-1] = (now, temperatura)
+            else:
+                storage[endereco].append((now, temperatura))
+        else:
+            storage[endereco].append((now, temperatura))
+        if len(storage[endereco]) > 1000:
+            storage[endereco].popleft()
+
+
+mqttc = mqtt.Client(client_id="", userdata=None, protocol=mqtt.MQTTv5)
+mqttc.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
 
 app = FastAPI()
-# Keep active WebSocket connections
-active_clients: List[WebSocket] = []
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+
+mqttc.username_pw_set(hivemq_user, hivemq_pass)
+mqttc.connect("74101a5cd6e04a08a2d360bb58565bea.s1.eu.hivemq.cloud", 8883, 60)
+
 
 # --- Enable CORS ---
 app.add_middleware(
@@ -21,7 +65,9 @@ app.add_middleware(
     allow_headers=["*"],       # Allow all headers
 )
 
-# In-memory storage: list of (endereco, temperatura, timestamp)
+templates = Jinja2Templates(directory="frontend")
+
+
 storage: Dict[str, Deque] = defaultdict(deque)
 
 # enderecos = {
@@ -29,34 +75,6 @@ storage: Dict[str, Deque] = defaultdict(deque)
 #     "sensor_2": "Sensor cima"
 # }
 
-
-async def broadcast_snapshot(obj):
-    """Send the latest snapshot to all connected clients."""
-    disconnected = []
-    for ws in active_clients:
-        try:
-            await ws.send_json(obj)
-        except Exception:
-            disconnected.append(ws)
-    # Remove broken connections
-    for ws in disconnected:
-        active_clients.remove(ws)
-
-@app.get("/log")
-async def log(endereco: str = Query(...), temperatura: float = Query(...)):
-    """Receive and log endereco, temperatura, and timestamp."""
-    now = datetime.now(timezone.utc)
-    # endereco = enderecos[endereco]
-    storage[endereco].append((now, temperatura))
-    if len(storage[endereco]) > 1000:
-        aleatorio = random.randint(0, 999)
-        del storage[endereco][aleatorio]
-    
-    
-    await broadcast_snapshot({
-        "endereco": endereco, "temperatura": temperatura, "data": now.isoformat()
-    })
-    return {"status": "ok", "endereco": endereco, "temperatura": temperatura, "time": now.isoformat()}
 
 class Base(BaseModel):
     endereco: str
@@ -80,43 +98,28 @@ async def log_post(data: List[Base]):
             storage[endereco].append((now, temperatura))
         if len(storage[endereco]) > 1000:
             storage[endereco].popleft()
-        await broadcast_snapshot({
-            "endereco": endereco, 
-            "temperatura": temperatura, 
-            "data": now.isoformat()
-        })
+        # await fast_mqtt.publish("/mqtt", {
+        #     "endereco": endereco, 
+        #     "temperatura": temperatura, 
+        #     "data": now.isoformat()
+        # })
         
     return {"status": "ok", "endereco": endereco, "temperatura": temperatura, "time": now.isoformat()}
-        
-    
-    
+
 
 @app.get("/dados")
 def dados():
     return [{"endereco": e, "temperatura": [temp for t, temp in vals], "data": [t.isoformat() for t, temp in vals]} for e, vals in storage.items()]
 
 
+@app.get('/', response_class=HTMLResponse)
+def index(req: Request):
+    return templates.TemplateResponse(
+        request=req, 
+        name="index.html",
+        context={
+            "hivemq_user": hivemq_user, 
+            "hivemq_pass": hivemq_pass
+    })
 
-
-
-
-@app.get('/')
-def index():
-    file = open('./frontend/index.html', 'r')
-    return HTMLResponse(content=file.read(), status_code=200)
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    active_clients.append(websocket)
-
-    try:
-        while True:
-            # keep connection alive (no incoming messages expected)
-            await websocket.receive_text()
-    except Exception:
-        pass
-    finally:
-        if websocket in active_clients:
-            active_clients.remove(websocket)
+mqttc.loop_start()
